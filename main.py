639b615168310aa7e324a74d9dcb9666d94e672d
Yang Lu <yangxlu@umich.edu>
2016-04-04 00:48:10 -0400

    Right colors

--- main.py
+++ main.py
@@ -14,39 +14,23 @@ class IndexHandler(webapp2.RequestHandler):
         logging.info("GET") 
         if self.request.path == '/aboutme.html':   
             template = JINJA_ENVIRONMENT.get_template('templates/aboutme.html')
-            self.response.write(template.render({'aboutme' : 'ABOUT ME', 'home' : 'Home', 'research' : 'Research', 'login':'Extras'}))
+            self.response.write(template.render({'aboutme' : 'ABOUT ME', 'home' : 'Home', 'research' : 'Research', 'login':'Maps', 'title': 'About Yang'}))
         elif self.request.path == '/research.html':   
             template = JINJA_ENVIRONMENT.get_template('templates/research.html')
-            self.response.write(template.render({'research' : 'RESEARCH', 'aboutme' : 'About Me', 'home' : 'Home', 'login':"Extras"}))
+            self.response.write(template.render({'research' : 'RESEARCH', 'aboutme' : 'About Me', 'home' : 'Home', 'login':"Maps", 'title': 'Research'}))
         elif self.request.path == '/feedback.html':   
             template = JINJA_ENVIRONMENT.get_template('templates/feedback.html')
-            self.response.write(template.render({'research' : 'Research', 'aboutme' : 'About Me', 'home' : 'Home', 'login':"Extras"}))
+            self.response.write(template.render({'research' : 'Research', 'aboutme' : 'About Me', 'home' : 'Home', 'login':"Maps", 'title': 'Feedback'}))
+        elif self.request.path == '/login.html':  
+            template = JINJA_ENVIRONMENT.get_template('templates/login.html')
+            self.response.write(template.render({'login': 'MAPS', 'title': 'Maps Page', 'home': 'Home', 'aboutme' : 'About Me', 'research' : 'Research'}))
         else:
             template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-            self.response.write(template.render({'home': 'HOME', 'aboutme' : 'About Me', 'research' : 'Research', 'login':'Extras'}))
+            self.response.write(template.render({'home': 'HOME', 'aboutme' : 'About Me', 'research' : 'Research', 'login':'Maps', 'title': 'Home'}))
 
-class LoginHandler(webapp2.RequestHandler):
-    def get(self):
-        logging.info("GET")
-        template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-        self.response.write(template.render({'login': 'EXTRAS', 'title': 'Login Page', 'home': 'Home', 'aboutme' : 'About Me', 'research' : 'Research'}))
-    def post(self):
-        nameGuess = self.request.get("name")
-        passGuess = self.request.get("pw")
-        logging.info("Username:" + nameGuess)
-        logging.info("Password:" + passGuess)
-        if (nameGuess == 'Colleen' and passGuess == 'pass'):
-            template = JINJA_ENVIRONMENT.get_template('templates/loginsuccess.html')
-            self.response.write(template.render({'home': 'Home','family':'Family', 'food': 'Food', 'login': 'LOGIN', 'title': ' Login Success!'}))
-            logging.info("Correct Credentials")
-        else: 
-            self.response.write("Bad Credentials. Please try again.")
-            template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-            self.response.write(template.render({'home': 'Home','family':'Family', 'food': 'Food', 'login': 'LOGIN', 'title': 'Login Page'}))
-            logging.info("Wrong Credentials")
+ 
            
 #depending on what url is inputed, display this page.
-app = webapp2.WSGIApplication([
-    ('/login.html', LoginHandler),
+app = webapp2.WSGIApplication([   
     ('/.*', IndexHandler)
 ], debug=True)
